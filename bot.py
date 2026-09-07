13f0a7cc057d7ffdfe3de3946ee220cca9485837
amirsetfin-web <amirsetfin@gmail.com>
2026-09-07 09:40:41 +0330

    Replace BOT_TOKEN and ADMIN_ID with hardcoded values
    Replaced environment variable loading for BOT_TOKEN and ADMIN_ID with hardcoded values.

--- bot.py
+++ bot.py
@@ -53,8 +53,8 @@ from telegram.ext import (
 # ---------------------------------------------------------------------------
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+BOT_TOKEN = os.getenv("8871054621:AAE9MgvinDQ0WrqOBC6TncQCcJD5vai6E7M")
+ADMIN_ID = int(os.getenv("AmirSetfin_YT", "0"))
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # روی سرور (مثل Railway) می‌تونی DATA_DIR رو به مسیر یه Volume دائمی ست کنی
